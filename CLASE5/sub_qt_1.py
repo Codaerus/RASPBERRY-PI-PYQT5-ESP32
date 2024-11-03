88f50b798afe0a376e0e7a053b7d4af97280fb66
Codaerus <christianqc91@gmail.com>
2024-11-03 10:53:48 -0500

    Create sub_qt_1.py

---
+++ CLASE5/sub_qt_1.py
@@ -0,0 +1,35 @@
+from PyQt5.QtWidgets import QWidget,QApplication
+import paho.mqtt.client as mqtt
+import sys
+
+class App(QWidget):
+	def __init__(self):
+	    super().__init__()
+      self.w = 600
+	    self.h = 300
+	    self.x = 100
+	    self.y = 100
+      self.titulo = "MONITOREO"
+	    self.initUI()
+      #Configuracion del broker
+	    client = mqtt.Client()
+      client.on_connect = self.on_connect
+	    client.on_message = self.on_message
+      #Conexion al broker
+	    client.connect("192.168.1.16", port=1883)
+	    client.loop_start()#Inicia bucle en un hilo separado
+	    #Callback que se ejecutar al conectar al broker
+	def on_connect(client, userdata, flags, rc):
+	    print("Conectado al broker MQTT")
+	    client.subscribe("canalx")
+	def on_message(client, userdata, message):
+	    printf("Mensaje recibido",message.payload.decode())
+	def initUI(self):
+	    self.setGeometry(self.x,self.y,self.w,self.h)
+	    self.setWindowTitle(self.titulo)
+	    self.show()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    ex = App()
+    sys.exit(app.exec_())
