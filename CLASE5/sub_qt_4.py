f282fa376c477284e83ac59d29da5ca30492c7b3
Codaerus <christianqc91@gmail.com>
2024-11-10 10:21:03 -0500

    Create sub_qt_4.py

---
+++ CLASE5/sub_qt_4.py
@@ -0,0 +1,48 @@
+from PyQt6.QtWidgets import QWidget, QApplication, QLabel
+import paho.mqtt.client as mqtt
+import sys
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.w = 600
+        self.h = 300
+        self.x = 100
+        self.y = 100
+        self.titulo = "MONITOREO"
+        self.initUI()
+
+        # Label
+        self.label1 = QLabel("Valor1:  ",self)
+        self.label1.move(100,100)
+        self.label2 = QLabel("Valor2:  ",self)
+        self.label2.move(100,150)
+        # Configuracion del broker
+        client = mqtt.Client()
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+
+        # Conexion al broker
+        client.connect("192.168.1.16", port=1883)
+        client.loop_start()  # Inicia bucle en un hilo separado
+        self.show()
+    # Callback que se ejecutar al conectar al broker
+    def on_connect(self, client, userdata, flags, rc):
+        print("Conectado al broker MQTT")
+        client.subscribe("canalx")
+
+    def on_message(self, client, userdata, message):
+        print("Mensaje recibido:", message.payload.decode())
+        self.label1.setText("Valor1: " + message.payload.decode().split(",")[0])
+        self.label1.adjustSize()
+        self.label2.setText("Valor2: " + message.payload.decode().split(",")[1])
+        self.label2.adjustSize()
+    def initUI(self):
+        self.setGeometry(self.x, self.y, self.w, self.h)
+        self.setWindowTitle(self.titulo)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    ex = App()
+    sys.exit(app.exec())
