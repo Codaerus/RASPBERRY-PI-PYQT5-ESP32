b80339d44401e9dc36e72d7dbcda4dd9562bc40c
Codaerus <christianqc91@gmail.com>
2025-03-29 10:10:19 -0500

    Add files via upload

--- CLASE5/sub_qt_3.py
+++ CLASE5/sub_qt_3.py
@@ -1,45 +1,43 @@
-from PyQt6.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 import paho.mqtt.client as mqtt
-import sys
 
 class App(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.w = 600
-        self.h = 300
-        self.x = 100
-        self.y = 100
-        self.titulo = "MONITOREO"
-        self.initUI()
-
-        # Label
-        self.label = QLabel("Valor:  ",self)
-        self.label.move(100,100)
-        
-        # Configuracion del broker
-        client = mqtt.Client()
-        client.on_connect = self.on_connect
-        client.on_message = self.on_message
-
-        # Conexion al broker
-        client.connect("192.168.1.16", port=1883)
-        client.loop_start()  # Inicia bucle en un hilo separado
-        self.show()
-    # Callback que se ejecutar al conectar al broker
-    def on_connect(self, client, userdata, flags, rc):
-        print("Conectado al broker MQTT")
-        client.subscribe("canalx")
-
-    def on_message(self, client, userdata, message):
-        print("Mensaje recibido:", message.payload.decode())
-        self.label.setText("Valor: " + message.payload.decode())
-        self.label.adjustSize()
-    def initUI(self):
-        self.setGeometry(self.x, self.y, self.w, self.h)
-        self.setWindowTitle(self.titulo)
-
+	def __init__(self):
+		super().__init__()
+		self.w = 600
+		self.h = 300
+		self.x = 100
+		self.y = 100
+		self.titulo = "MONITOREO"
+		self.initUI()
+	def on_connect(self,client,userdata,flags,rc):
+		print("Conectado al broker MQTT")
+		client.subscribe("canalx")
+	def on_message(self,client,userdata,message):
+		pot1 = message.payload.decode().split(",")[0]
+		pot2 = message.payload.decode().split(",")[1]
+		self.label1.setText("Valor: "+pot1)
+		self.label1.adjustSize()
+		self.label2.setText("Valor:"+pot2)
+		self.label2.adjustSize()
+	def initUI(self):
+		self.setGeometry(self.x,self.y,self.w,self.h)
+		self.setWindowTitle(self.titulo)
+		#QLabel 
+		self.label1 = QLabel("Valor:   ",self)
+		self.label1.move(100,100)
+		
+		self.label2 = QLabel("Valor:   ", self)
+		self.label2.move(250,100)
+		#Configuracin del broker
+		client = mqtt.Client()
+		client.on_connect = self.on_connect
+		client.on_message = self.on_message
+		client.connect("192.168.1.14",port=1883)
+		client.loop_start()
+		self.show()
 
 if __name__ == "__main__":
-    app = QApplication([])
-    ex = App()
-    sys.exit(app.exec())
+	app = QApplication([])
+	ex = App()
+	app.exec_()
